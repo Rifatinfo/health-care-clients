bab72afd83f59339830530d3b761769a12cc66eb
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-11-06 17:57:55 +0600

    first commit

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,29 @@
+import { Hero } from "@/components/modules/Home/Hero";
+import Specialities from "@/components/modules/Specialties/Specialties";
+import Steps from "@/components/modules/Steps/Steps";
+import Testimonials from "@/components/modules/Testimonials/Testimonials";
+import TopRatedDoctors from "@/components/modules/TopRatedDoctors/TopRatedDoctors";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Hero />
+        <Specialities />
+        <TopRatedDoctors />
+        <Steps />
+        <Testimonials />
+      </main>
+    </>
+  );
+}
