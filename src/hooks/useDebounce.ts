95200652c3f6e564b805f5f7bebcacbed7987eaa
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-11-15 20:21:22 +0600

    add use debounced function

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 export function useDebounce<T>(value: T, delay: number = 500): T {
     const [debounceValue, setDebounceValue] = useState<T>(value);
 
