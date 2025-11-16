03921764005a9778e558fa31b108bce010a5dfbd
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-11-17 00:18:44 +0600

    add pagination complete function

--- src/components/shared/TablePagination.tsx
+++ src/components/shared/TablePagination.tsx
@@ -38,6 +38,47 @@ const TablePagination = ({currentPage, totalPages} :  TablePaginationProps) => {
                 <ChevronLeft className="h-4 w-4 mr-1"/>
                 Previous
             </Button>
+
+            <div className="flex items-center gap-1">
+              {
+                Array.from({length : Math.min(5, totalPages)},
+                (_, index) => {
+                    let pageNumber;
+                    
+                    if(totalPages <= 5){
+                       pageNumber = index + 1;
+                    } else if(currentPage <= 3){
+                      pageNumber = index + 1;
+                    } else if(currentPage >= totalPages - 2){
+                        pageNumber = totalPages - 4 + index;
+                    }else {
+                        pageNumber = currentPage - 2 + index;
+                    }
+
+
+                    return(
+                        <Button
+                        key={pageNumber}
+                        variant={pageNumber === currentPage ? "default" : "outline"}
+                        onClick={() => navigationToPage(pageNumber)}
+                        disabled={isPending}
+                        className="w-10"
+                        >
+                            {pageNumber}
+                        </Button>
+                    )
+                }
+            
+            )
+              }
+            </div>
+
+            <Button
+            variant="outline"
+            size="sm"
+            onClick={() => navigationToPage(currentPage +1)}
+            disabled={currentPage === totalPages || isPending}
+            >Next</Button>
         </div>
     );
 };
