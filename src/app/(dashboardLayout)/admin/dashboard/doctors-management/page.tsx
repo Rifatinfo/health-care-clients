6019ea544f3f50ec5e83b15eaed7aaaaec4a5443
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-11-19 19:11:04 +0600

    add admin-doctor-mangement

---
+++ src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -0,0 +1,56 @@
+import DoctorsManagementHeader from "@/components/Admin/DoctorsManagement/DoctorsManagementHeader";
+import DoctorsTable from "@/components/Admin/DoctorsManagement/DoctorsTable";
+import RefreshButton from "@/components/shared/RefreshButton";
+import SearchFilter from "@/components/shared/SearchFilter";
+// import SelectFilter from "@/components/shared/SelectFilter";
+import TablePagination from "@/components/shared/TablePagination";
+import TableSkeleton from "@/components/shared/TableSkeleton";
+import { queryStringFormatter } from "@/components/ui/formatters";
+import { getDoctors } from "@/services/admin/doctorManagement";
+import { getSpecialities } from "@/services/admin/specialitiesManagement";
+// import { ISpecialty } from "@/types/specialities.interface";
+import { Suspense } from "react";
+
+const AdminDoctorsManagementPage = async ({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+    const searchParamsObj = await searchParams;
+    const queryString = queryStringFormatter(searchParamsObj); // {searchTerm: "John", speciality: "Cardiology" => "?searchTerm=John&speciality=Cardiology"}
+    const specialitiesResult = await getSpecialities();
+    const doctorsResult = await getDoctors(queryString);
+    const totalPages = Math.ceil(
+        (doctorsResult?.meta?.total ?? 0) / (doctorsResult?.meta?.limit ?? 1)
+    );
+    return (
+        <div className="space-y-6">
+            <DoctorsManagementHeader specialities={specialitiesResult.data} />
+            <div className="flex space-x-2">
+                <SearchFilter paramName="searchTerm" placeholder="Search doctors..." />
+                {/* <SelectFilter
+                    paramName="speciality" // ?speciality="Cardiology"
+                    options={specialitiesResult.data.map((speciality: ISpecialty) => ({
+                        label: speciality.title,
+                        value: speciality.title,
+                    }))}
+                    placeholder="Filter by speciality"
+                /> */}
+                <RefreshButton />
+            </div>
+            <Suspense fallback={<TableSkeleton columns={10} rows={10} />}>
+                <DoctorsTable
+                    doctors={doctorsResult.data}
+                    specialities={specialitiesResult.data}
+                />
+                <TablePagination
+                    currentPage={doctorsResult?.meta?.page ?? 1}
+                    totalPages={totalPages}
+                />
+
+            </Suspense>
+        </div>
+    );
+};
+
+export default AdminDoctorsManagementPage;
